384dab95266dfc33024e02bcba5ebc42b6c94ade
Henrique Watanabe <henrique.watpequim@gmail.com>
2024-07-30 21:25:35 -0300

    GeradorPWM para teste speeduino porta analogica

---
+++ Speeduino_Tester/GeradorPwm/GeradorPwm.ino
@@ -0,0 +1,20 @@
+#define PIN_OUT 3
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+}
+int value = 0;
+void loop() {
+  String sData = "";
+  // put your main code here, to run repeatedly:
+  
+  sData = Serial.readString(); // ler a string
+  
+  if(sData.length() > 0){
+    value = sData.toInt();
+    analogWrite(PIN_OUT, value);
+    Serial.println("valor: " + String(value));
+  }
+
+}
