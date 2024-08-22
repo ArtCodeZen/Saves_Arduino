6e02e680af30130ae84276597d311131252a3d54
Henrique Watanabe <henrique.watpequim@gmail.com>
2024-08-22 10:24:25 -0300

    adicionado getInterval e updateInterval

--- Arduino_POO/TimerDelayWithStruct/TimerDelayWithStruct.ino
+++ Arduino_POO/TimerDelayWithStruct/TimerDelayWithStruct.ino
@@ -4,7 +4,7 @@ struct TimerDelay{
   unsigned long previousMillis;
   unsigned long interval;
   // constructor
-  TimerDelay(unsigned long interval): previousMillis(0), interval(interval){}
+  TimerDelay(int interval): previousMillis(0), interval(interval){}
   bool check(){
     unsigned long currentMillis = millis();
     if(currentMillis - previousMillis >= interval){
@@ -13,20 +13,30 @@ struct TimerDelay{
     }
     return false;
   }
+  void updateInterval(unsigned long newInterval){
+    if(newInterval >= 1)
+      interval = newInterval;
+      
 
+  }
+  int getCurrentInterval(){
+    return interval;
+  }
 };
 
-TimerDelay myTimer(500);
+TimerDelay myTimer(400);
 void setup() {
   // put your setup code here, to run once:
   pinMode(PIN, OUTPUT);
-
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(myTimer.check()){
     digitalWrite(PIN, !digitalRead(PIN));
+    myTimer.updateInterval(myTimer.getCurrentInterval() - 1);
+    Serial.println(myTimer.getCurrentInterval());
+
   }
-  
 }
